132ba40e207c5efa99f5c7ad2c04bacaaac64820
Andres Diaz <diazccx@gmail.com>
2024-10-26 16:38:34 -0500

    Creat component Home

---
+++ src/components/pages/home/Home.page.vue
@@ -0,0 +1,7 @@
+<template>
+    <HomeTemplate/>
+</template>
+<script setup lang="ts">
+import HomeTemplate from '../../templates/home/Home.template.vue';
+</script>
+<style scoped></style>
---
+++ src/components/templates/home/Home.template.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="home">
+        <header>
+            header
+        </header>
+        <section>
+            section
+        </section>
+        <footer>
+            footer
+        </footer>
+    </main>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped src="./Home.template.scss"></style>
